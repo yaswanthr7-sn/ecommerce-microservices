71bd50031b65cd000f53a0ec0897ce95efb5174e
yaswanthr7-sn <yaswanthr7@gmail.com>
2026-08-10 18:29:59 +0530

    Code Rabbit review findings fix

--- services/product-service/src/main/java/com/yaswanth/ecommerce/product/component/LoggingAspect.java
+++ services/product-service/src/main/java/com/yaswanth/ecommerce/product/component/LoggingAspect.java
@@ -2,10 +2,8 @@ package com.yaswanth.ecommerce.product.component;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -16,18 +14,18 @@ public class LoggingAspect {
     @Around("execution(* com.yaswanth.ecommerce.product.service..*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
 
-        Object result = joinPoint.proceed();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long elapsed = System.nanoTime() - start;
 
-        long end = System.currentTimeMillis();
-
-        log.info(
-                "Method {} executed in {} ms",
-                joinPoint.getSignature().toShortString(),
-                end - start
-        );
-
-        return result;
+            log.info(
+                    "Method {} executed in {} ms",
+                    joinPoint.getSignature().toShortString(),
+                    elapsed / 1_000_000.0
+            );
+        }
     }
 }
