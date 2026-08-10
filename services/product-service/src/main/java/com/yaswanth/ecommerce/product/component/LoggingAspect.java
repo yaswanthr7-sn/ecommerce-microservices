55ffcaa5b13317fc21f8c02e8bbe62ca0a9d7dc2
yaswanthr7-sn <yaswanthr7@gmail.com>
2026-08-10 18:12:48 +0530

    feat: add AOP execution logging

---
+++ services/product-service/src/main/java/com/yaswanth/ecommerce/product/component/LoggingAspect.java
@@ -0,0 +1,33 @@
+package com.yaswanth.ecommerce.product.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("execution(* com.yaswanth.ecommerce.product.service..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        log.info(
+                "Method {} executed in {} ms",
+                joinPoint.getSignature().toShortString(),
+                end - start
+        );
+
+        return result;
+    }
+}
