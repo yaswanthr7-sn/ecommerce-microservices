9850043937b634878c16a842b141cc2dbca8ad61
yaswanthr7-sn <yaswanthr7@gmail.com>
2026-08-11 07:39:57 +0530

    Order service, circuit breaker

---
+++ services/order-service/src/main/java/com/yaswanth/ecommerce/order/model/PaymentRequest.java
@@ -0,0 +1,25 @@
+package com.yaswanth.ecommerce.order.model;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class PaymentRequest {
+
+    @NotNull
+    private UUID orderId;
+
+    @NotNull
+    @DecimalMin(value = "0.01")
+    @Digits(integer = 17, fraction = 2)
+    private BigDecimal amount;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String currency;
+}
