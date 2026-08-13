442d8d1b3f5cfb396e7836a5f798f93a04222502
yaswanthr7-sn <yaswanthr7@gmail.com>
2026-08-13 15:51:06 +0530

    caching

--- services/product-service/src/main/java/com/yaswanth/ecommerce/product/ProductServiceApplication.java
+++ services/product-service/src/main/java/com/yaswanth/ecommerce/product/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package com.yaswanth.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ProductServiceApplication {
 
